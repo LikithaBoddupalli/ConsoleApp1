7584907ad31aa7a0483667a21d439bf6854b20ca
Likitha <Likitha@LAPTOP-QJ5988CH>
2023-09-07 12:29:22 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+String name = "Likitha";
+Console.WriteLine("Hello " + name);
+
+Console.WriteLine($"Hii {name}");
+
+String n1 = "Varshu";
+String n2 = "Likki";
+Console.WriteLine($"My Friends are {n1},{n2}");
+Console.WriteLine($"Length of n1 is {n1.Length}");
+
+
+
+
