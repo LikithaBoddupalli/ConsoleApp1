e1dd291991d658acac5191f4f91c32fdf4f5b733
LikithaBoddupalli <136110518+LikithaBoddupalli@users.noreply.github.com>
2023-09-07 12:50:44 +0530

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,7 +9,13 @@ Console.WriteLine($"Hii {name}");
 String n1 = "Varshu";
 String n2 = "Likki";
 Console.WriteLine($"My Friends are {n1},{n2}");
+//Getting length of string
 Console.WriteLine($"Length of n1 is {n1.Length}");
+
+//Triming the spaces
+String greet = "        Hello...        ";
+Console.WriteLine($"[{greet}]");
+Console.WriteLine($"[{greet.TrimStart()}");
 
 
 
