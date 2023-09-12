23788cb8a90d13896beff6bd9402dfe480a93b06
LikithaBoddupalli <136110518+LikithaBoddupalli@users.noreply.github.com>
2023-09-12 14:32:36 +0530

    Update BranchesAndLoops.cs

--- BranchesAndLoops.cs
+++ BranchesAndLoops.cs
@@ -45,39 +45,27 @@ else
 }
 
 // else if statements
-DisplayCharacter('f');  // Output: A lowercase letter: f
-DisplayCharacter('R');  // Output: An uppercase letter: R
-DisplayCharacter('8');  // Output: A digit: 8
-DisplayCharacter(',');  // Output: Not alphanumeric character: ,
 
-void DisplayCharacter(char ch)
+Console.WriteLine("Enter the number:");      
+int number = int.Parse(Console.ReadLine());
+if(number == 1) 
 {
-    if (char.IsUpper(ch))
-    {
-        Console.WriteLine($"An uppercase letter: {ch}");
-    }
-    else if (char.IsLower(ch))
-    {
-        Console.WriteLine($"A lowercase letter: {ch}");
-    }
-    else if (char.IsDigit(ch))
-    {
-        Console.WriteLine($"A digit: {ch}");
-    }
-    else
-    {
-        Console.WriteLine($"Not alphanumeric character: {ch}");
-    }
+    Console.WriteLine("One");
+}
+else if(number == 2)
+{
+    Console.WriteLine("two");
+}
+else
+{
+    Console.WriteLine("Other than 1 and 2");
 }
 
 //switch statement
-DisplayMeasurement(-4);  // Output: Measured value is -4; too low.
-DisplayMeasurement(5);  // Output: Measured value is 5.
-DisplayMeasurement(30);  // Output: Measured value is 30; too high.
-DisplayMeasurement(double.NaN);  // Output: Failed measurement.
 
-void DisplayMeasurement(double measurement)
-{
+Console.WriteLine("Enter the number:");      
+double measurement = double.Parse(Console.ReadLine());
+
     switch (measurement)
     {
         case < 0.0:
@@ -96,14 +84,11 @@ void DisplayMeasurement(double measurement)
             Console.WriteLine($"Measured value is {measurement}.");
             break;
     }
-}
 
 //case guard statements
-DisplayMeasurements(3, 4);  // Output: First measurement is 3, second measurement is 4.
-DisplayMeasurements(5, 5);  // Output: Both measurements are valid and equal to 5.
-
-void DisplayMeasurements(int a, int b)
-{
+Console.WriteLine("Enter two number:");      
+int a = int.Parse(Console.ReadLine());
+int b = double.Parse(Console.ReadLine());
     switch ((a, b))
     {
         case (> 0, > 0) when a == b:
@@ -118,7 +103,7 @@ void DisplayMeasurements(int a, int b)
             Console.WriteLine("One or both measurements are not valid.");
             break;
     }
-}
+
 
 //While Loop
 int count = 0;
