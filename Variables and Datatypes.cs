69d71b1a3f921cdd23502316f28f669e30bfda85
LikithaBoddupalli <136110518+LikithaBoddupalli@users.noreply.github.com>
2023-09-13 10:18:34 +0530

    Update Variables and Datatypes.cs

--- Variables and Datatypes.cs
+++ Variables and Datatypes.cs
@@ -26,25 +26,7 @@ namespace CSharp
                     Console.WriteLine(boolean);
               
                     const float Pi = 3.14f;
-                    Console.WriteLine(Pi);       
-              
-                   //implicit type conversion
-                     byte b = 1;
-                     int i = b;
-                     Console.WriteLine(i);
-                     float f = i;
-                     Console.WriteLine(f);
-              
-                    //expicit type conversion
-                       byte by =(byte) i;
-                      Console.WriteLine(by);
-              
-                    //non-compatible types
-                      var number = "1234";
-                      int i = Convert.ToInt32(number);
-                      double d=Convert.ToInt64(number);
-                      Console.WriteLine(i);
-                      Console.WriteLine(d);
+                    Console.WriteLine(Pi);   
               
                   //Arithmetic operations
                   int a = 18;
