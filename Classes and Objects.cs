09cfce49dfa7c57f799a7b3565ba7c1147e647d3
LikithaBoddupalli <136110518+LikithaBoddupalli@users.noreply.github.com>
2023-09-13 15:22:06 +0530

    Update Classes and Objects.cs

--- Classes and Objects.cs
+++ Classes and Objects.cs
@@ -1,1 +1,40 @@
+//creating a HumanClass
+namespace CSharp
+{
+    class HumanClass
+    {
+       public string firstName = "likitha";
+
+         public void talk()
+        {
+            Console.WriteLine("Talking");
+        }
+        public static void eat()
+        {
+            Console.WriteLine("Eating");
+        }
+    }
+}
+
+
+namespace CSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //create an object of HumanClass
+            HumanClass myObj = new HumanClass();
+            HumanClass humanClass = new HumanClass();
+            humanClass.firstName = "Bunny";
+         
+            //access the variables of the classes
+            Console.WriteLine(myObj.firstName);
+            Console.WriteLine(humanClass.firstName);
+        }
+    }
+ }
+
+
+ 
 
