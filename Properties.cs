81d9a7eda2e8ccc18b4be8f081108b7e03662478
LikithaBoddupalli <136110518+LikithaBoddupalli@users.noreply.github.com>
2023-09-19 15:02:33 +0530

    Update Properties.cs

--- Properties.cs
+++ Properties.cs
@@ -81,3 +81,33 @@ public class MainClass {
         Console.WriteLine(myRide.NumTires);
     }
 }
+
+
+//read-only property
+  public class Employee  
+    {  
+        private static int counter;  
+  
+        public Employee()  
+        {  
+            counter++;  
+        }  
+        public static int Counter  
+        {  
+            get  
+            {  
+                return counter;  
+            }  
+         }  
+   }  
+   class TestEmployee{  
+       public static void Main(string[] args)  
+        {  
+            Employee e1 = new Employee();  
+            Employee e2 = new Employee();  
+            Employee e3 = new Employee();  
+            //e1.Counter = 10;//Compile Time Error: Can't set value  
+  
+            Console.WriteLine("No. of Employees: " + Employee.Counter);  
+        }  
+    }  
